75b11cec456b6da7998baa03fac708d8a92e0bcb
geniuslyy <genius_li@163.com>
2013-07-17 21:50:42 +0800

    跟上次一样……

--- src/com/kingdee/spider/handlers/AccessASTVisitor.java
+++ src/com/kingdee/spider/handlers/AccessASTVisitor.java
@@ -1,87 +1,19 @@
 package com.kingdee.spider.handlers;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.ArrayAccess;
-import org.eclipse.jdt.core.dom.ArrayCreation;
-import org.eclipse.jdt.core.dom.ArrayInitializer;
-import org.eclipse.jdt.core.dom.ArrayType;
-import org.eclipse.jdt.core.dom.AssertStatement;
-import org.eclipse.jdt.core.dom.Assignment;
-import org.eclipse.jdt.core.dom.Block;
-import org.eclipse.jdt.core.dom.BlockComment;
-import org.eclipse.jdt.core.dom.BooleanLiteral;
-import org.eclipse.jdt.core.dom.BreakStatement;
-import org.eclipse.jdt.core.dom.CastExpression;
-import org.eclipse.jdt.core.dom.CatchClause;
-import org.eclipse.jdt.core.dom.CharacterLiteral;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.ConditionalExpression;
 import org.eclipse.jdt.core.dom.ConstructorInvocation;
-import org.eclipse.jdt.core.dom.ContinueStatement;
-import org.eclipse.jdt.core.dom.DoStatement;
-import org.eclipse.jdt.core.dom.EmptyStatement;
-import org.eclipse.jdt.core.dom.EnhancedForStatement;
-import org.eclipse.jdt.core.dom.EnumConstantDeclaration;
-import org.eclipse.jdt.core.dom.EnumDeclaration;
-import org.eclipse.jdt.core.dom.ExpressionStatement;
-import org.eclipse.jdt.core.dom.FieldAccess;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
-import org.eclipse.jdt.core.dom.ForStatement;
-import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.ImportDeclaration;
-import org.eclipse.jdt.core.dom.InfixExpression;
 import org.eclipse.jdt.core.dom.Initializer;
-import org.eclipse.jdt.core.dom.InstanceofExpression;
-import org.eclipse.jdt.core.dom.Javadoc;
-import org.eclipse.jdt.core.dom.LabeledStatement;
-import org.eclipse.jdt.core.dom.LineComment;
-import org.eclipse.jdt.core.dom.MarkerAnnotation;
-import org.eclipse.jdt.core.dom.MemberRef;
-import org.eclipse.jdt.core.dom.MemberValuePair;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.MethodRef;
 import org.eclipse.jdt.core.dom.MethodRefParameter;
-import org.eclipse.jdt.core.dom.Modifier;
-import org.eclipse.jdt.core.dom.NormalAnnotation;
-import org.eclipse.jdt.core.dom.NullLiteral;
-import org.eclipse.jdt.core.dom.NumberLiteral;
 import org.eclipse.jdt.core.dom.PackageDeclaration;
-import org.eclipse.jdt.core.dom.ParameterizedType;
-import org.eclipse.jdt.core.dom.ParenthesizedExpression;
-import org.eclipse.jdt.core.dom.PostfixExpression;
-import org.eclipse.jdt.core.dom.PrefixExpression;
-import org.eclipse.jdt.core.dom.PrimitiveType;
-import org.eclipse.jdt.core.dom.QualifiedName;
-import org.eclipse.jdt.core.dom.QualifiedType;
-import org.eclipse.jdt.core.dom.ReturnStatement;
-import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.SimpleType;
-import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
-import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
-import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
-import org.eclipse.jdt.core.dom.SuperFieldAccess;
 import org.eclipse.jdt.core.dom.SuperMethodInvocation;
-import org.eclipse.jdt.core.dom.SwitchCase;
-import org.eclipse.jdt.core.dom.SwitchStatement;
-import org.eclipse.jdt.core.dom.SynchronizedStatement;
-import org.eclipse.jdt.core.dom.TagElement;
-import org.eclipse.jdt.core.dom.TextElement;
-import org.eclipse.jdt.core.dom.ThisExpression;
-import org.eclipse.jdt.core.dom.ThrowStatement;
-import org.eclipse.jdt.core.dom.TryStatement;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclarationStatement;
-import org.eclipse.jdt.core.dom.TypeLiteral;
-import org.eclipse.jdt.core.dom.TypeParameter;
-import org.eclipse.jdt.core.dom.UnionType;
-import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
-import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
-import org.eclipse.jdt.core.dom.WhileStatement;
-import org.eclipse.jdt.core.dom.WildcardType;
 
 public class AccessASTVisitor extends ASTVisitor {
 
@@ -92,415 +24,85 @@ public class AccessASTVisitor extends ASTVisitor {
 	}
 
 	@Override
-	public boolean visit(ArrayAccess node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ArrayCreation node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ArrayInitializer node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ArrayType node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(AssertStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(Assignment node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(Block node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(BlockComment node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(BooleanLiteral node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(BreakStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(CastExpression node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(CatchClause node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(CharacterLiteral node) {
-		return super.visit(node);
-	}
-
-	@Override
 	public boolean visit(ClassInstanceCreation node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(CompilationUnit node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ConditionalExpression node) {
+		message.append("ClassInstanceCreation:\t" + node.toString() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(ConstructorInvocation node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ContinueStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(DoStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(EmptyStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(EnhancedForStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(EnumConstantDeclaration node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(EnumDeclaration node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ExpressionStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(FieldAccess node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(FieldDeclaration node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ForStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(IfStatement node) {
+		message.append("ConstructorInvocation:\t" + node.toString() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(ImportDeclaration node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(InfixExpression node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(InstanceofExpression node) {
+		message.append("ImportDeclaration:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(Initializer node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(Javadoc node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(LabeledStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(LineComment node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(MarkerAnnotation node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(MemberRef node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(MemberValuePair node) {
+		message.append("Initializer:\t" + node.toString() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(MethodRef node) {
-		message.append("MethodRef:\t"+node.getName()+StringUtil.LINE_SEPARATOR);
+		message.append("MethodRef:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(MethodRefParameter node) {
-		message.append("MethodRefParameter:\t"+node.getName()+StringUtil.LINE_SEPARATOR);
+		message.append("MethodRefParameter:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(MethodDeclaration node) {
-		message.append("MethodDeclaration:\t"+node.getName()+StringUtil.LINE_SEPARATOR);
+		message.append("MethodDeclaration:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(MethodInvocation node) {
-		message.append("MethodInvocation:\t"+node.getName()+StringUtil.LINE_SEPARATOR);
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(Modifier node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(NormalAnnotation node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(NullLiteral node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(NumberLiteral node) {
+		message.append("MethodInvocation:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(PackageDeclaration node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ParameterizedType node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ParenthesizedExpression node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(PostfixExpression node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(PrefixExpression node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(PrimitiveType node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(QualifiedName node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(QualifiedType node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ReturnStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SimpleName node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SimpleType node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SingleMemberAnnotation node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SingleVariableDeclaration node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(StringLiteral node) {
+		message.append("PackageDeclaration:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(SuperConstructorInvocation node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SuperFieldAccess node) {
+		message.append("SuperConstructorInvocation:\t" + node.toString() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(SuperMethodInvocation node) {
-		message.append("SuperMethodInvocation:\t"+node.getName()+StringUtil.LINE_SEPARATOR);
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SwitchCase node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SwitchStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(SynchronizedStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(TagElement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(TextElement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ThisExpression node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(ThrowStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(TryStatement node) {
+		message.append("SuperMethodInvocation:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
 
 	@Override
 	public boolean visit(TypeDeclaration node) {
-		message.append("TypeDeclaration:\t"+node.getName()+StringUtil.LINE_SEPARATOR);
+		message.append("TypeDeclaration:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
+	}
+
+	@Override
+	public void endVisit(TypeDeclaration node) {
+		message.append("endVisit TypeDeclaration:\t" + node.getName() + StringUtil.LINE_SEPARATOR);
 	}
 
 	@Override
 	public boolean visit(TypeDeclarationStatement node) {
+		message.append("TypeDeclarationStatement:\t" + node.toString() + StringUtil.LINE_SEPARATOR);
 		return super.visit(node);
 	}
-
-	@Override
-	public boolean visit(TypeLiteral node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(TypeParameter node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(UnionType node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(VariableDeclarationExpression node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(VariableDeclarationStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(VariableDeclarationFragment node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(WhileStatement node) {
-		return super.visit(node);
-	}
-
-	@Override
-	public boolean visit(WildcardType node) {
-		return super.visit(node);
-	}
-	
-	
 }
